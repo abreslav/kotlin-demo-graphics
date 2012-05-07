cd8121617be3d26dd70cb646c16ac29d810b732b
Andrey Breslav <andrey.breslav@jetbrains.com>
2012-05-07 04:11:43 +0200

    Arrows added

--- src/main.kt
+++ src/main.kt
@@ -8,10 +8,18 @@ import java.util.TimerTask
 import com.sun.tools.javac.resources.legacy
 import javax.swing.JFrame
 import java.awt.Graphics2D
+import java.awt.Component
+import java.util.WeakHashMap
+import java.awt.event.MouseAdapter
+import java.awt.event.MouseEvent
+import java.util.List
+import java.util.ArrayList
+import java.util.Random
+import java.awt.Color
 
 class Wheel(
         var center : Point,
-        var radius : Int,
+        var radius : Double,
         var offset : Int,
         var angle : Double = 0.0
     ) {
@@ -19,15 +27,15 @@ class Wheel(
         get() = center + polarPoint(offset.toDouble(), angle)
 }
 
-fun main(args : Array<String>) {
+fun wheels() {
     var wheel1 = Wheel(
             center = Point(450, 200),
-            radius = 100,
+            radius = 100.0,
             offset = 70
     )
     var wheel2 = Wheel(
             center = Point(200, 200),
-            radius = 100,
+            radius = 100.0,
             offset = 70
     )
     val frame = mainFrame("Demo") {
@@ -38,10 +46,10 @@ fun main(args : Array<String>) {
                 drawCircle(center, radius)
 
                 for (i in 1..4) {
-                    drawLine((center - Point(radius, 0)) to center rotateAroundB (i * 45.deg + angle) ofLength radius * 2 )
+                    drawLine((center - Point(radius, 0.0)) to center rotateAroundB (i * 45.deg + angle) ofLength radius * 2 )
                 }
 
-                drawCircle(dotCenter, 10)
+                drawCircle(dotCenter, 10.0)
             }
 
             wheel1.draw()
@@ -66,10 +74,58 @@ fun main(args : Array<String>) {
     frame.show()
 }
 
+fun main(args : Array<String>) {
+    val width = 800
+    val height = 600
+    val arrows = Array(10) {
+        randomSegment(width / 2, height / 2, 100, 150)
+    }
+    val frame = mainFrame("Arrows") {
+        setSize(width, height)
+        val panel = drawPanel {
+            for (arr in arrows) {
+                // Arrows
+//                setColor(Color.BLACK)
+//                drawLine(arr)
+//                val da = 7.deg
+//                setColor(Color.RED)
+//                val r = 30.0
+//                drawLine(arr.b, arr.b + polarPoint(r, arr.angle - da - PI))
+//                drawLine(arr.b, arr.b + polarPoint(r, arr.angle + da - PI))
+
+                // Compass circles
+//                val r = (arr.abs / 2).i
+//                val middle = arr.middle
+//                val diam = middle to arr.a ofLength arr.abs
+//                setColor(Color.RED)
+//                fillArc(diam.middle.x.i - r, diam.middle.y.i - r, r * 2, r * 2, -diam.angle.toDeg(), 180)
+//                setColor(Color.BLUE)
+//                fillArc(diam.middle.x.i - r, diam.middle.y.i - r, r * 2, r * 2, -diam.angle.toDeg(), -180)
+//                setColor(Color.BLACK)
+//                fillCircle(middle, 10.0)
+//                drawLine(diam)
+//                drawCircle(arr.a, r.toDouble())
+            }
+        }.mouse
+            .moved { e ->
+                val p = Point(e.getPoint()!!)
+                for (i in arrows.indices) {
+                    val arr = arrows[i]
+                    arrows[i] = arr ofAngle ((p - arr.a).angle)
+                }
+                repaint()
+            }
+            .done
+        add(panel)
+    }
+    frame.show()
+}
+
 
 //////////////
 
 class Point(val x : Double, val y : Double)
+fun Point(p : java.awt.Point) = Point(p.x, p.y)
 fun Point(x : Int, y : Int) = Point(x.toDouble(), y.toDouble())
 fun Point.rotate(dAngle : Double) = polarPoint(abs, angle + dAngle)
 val Point.abs : Double
@@ -81,15 +137,18 @@ val Point.norm : Point
     get() = Point(x / abs, y / abs)
 
 fun Point.minus(p : Point) = Point(x - p.x, y - p.y)
-fun Point.times(length : Int) = Point(x * length, y * length)
-fun Point.plus(length : Int) = Point(x + length, y + length)
-fun Point.minus(length : Int) = Point(x - length, y - length)
+fun Point.times(length : Double) = Point(x * length, y * length)
+fun Point.div(factor : Double) = Point(x / factor, y / factor)
+fun Point.plus(length : Double) = Point(x + length, y + length)
+fun Point.minus(length : Double) = Point(x - length, y - length)
 
-fun polarPoint(r : Double, angle : Double) = Point(r * cos(angle), -r * sin(angle))
+fun polarPoint(r : Double, angle : Double) = Point(r * cos(angle), r * sin(angle))
 
 val Point.angle : Double
     get() = atan2(y, x)
 
+val random = Random()
+fun randomPoint(maxX : Int, maxY : Int) = Point(random.nextInt(maxX), random.nextInt(maxY))
 
 ////////////////////////
 
@@ -97,14 +156,43 @@ class Segment(val a : Point, val b : Point)
 
 fun Point.to(other : Point) = Segment(this, other)
 
-fun Segment.ofLength(length : Int) = Segment(a, a + (b - a).norm * length)
+fun Segment.ofLength(length : Double) = Segment(a, a + (b - a).norm * length)
 
 fun Segment.rotateAroundB(angle : Double) = Segment(b + ((a - b) rotate angle), b)
 
+fun randomSegment(maxX : Int, maxY : Int, minR : Int, maxR : Int) : Segment{
+    val a = randomPoint(maxX, maxY)
+    val ang = random.nextDouble() * 2 * PI
+    val r = minR + random.nextDouble() * (maxR - minR)
+    return Segment(a, a + polarPoint(r, ang))
+
+}
+
+fun Segment.toPoint() = b - a
+
+val Segment.angle : Double
+    get() = toPoint().angle
+
+val Segment.abs : Double
+    get() = toPoint().abs
+
+val Segment.middle : Point
+    get() = a + toPoint() / 2.0
+
+fun Segment.ofAngle(ang : Double) = Segment(a, a + polarPoint(abs, ang))
+
 ///////////////////////
 
-fun Graphics.drawCircle(center : demo.Point, r : Int) {
-    drawOval(center.x.toInt() - r, center.y.toInt() - r, r * 2, r * 2)
+///////////////////////
+
+fun Graphics.drawCircle(center : demo.Point, r : Double) {
+    val intR = (r * 2).toInt()
+    drawOval((center.x - r).toInt(), (center.y - r).toInt(), intR, intR)
+}
+
+fun Graphics.fillCircle(center : demo.Point, r : Double) {
+    val intR = (r * 2).toInt()
+    fillOval((center.x - r).toInt(), (center.y - r).toInt(), intR, intR)
 }
 
 fun Graphics.drawLine(a : Point, b : Point) {
@@ -124,6 +212,10 @@ val Double.deg : Double
 val Int.deg : Double
     get() = this.toDouble().deg
 
+val Double.i : Int
+    get() = toInt()
+
+fun Double.toDeg() = (this * 180.0 / PI).i
 
 /////////////////
 
@@ -143,6 +235,79 @@ fun drawPanel(paint : Graphics2D.() -> Unit) : JPanel {
         }
     }
 }
+
+
+val Component.mouse : Mouse
+    get() = Mouse(this)
+
+
+class MEvent {
+    private val handlers : List<(MouseEvent) -> Unit> = ArrayList()
+
+    fun fire(e : MouseEvent) {
+        for (handler in handlers) {
+            handler(e)
+        }
+    }
+
+    fun add(handler : (MouseEvent) -> Unit) {
+        handlers add handler
+    }
+}
+
+class Mouse(private val component : Component) {
+
+    private val clicked = MEvent();
+    private val moved = MEvent();
+    private val released = MEvent();
+    private val pressed = MEvent();
+
+    {
+        val listener = object : MouseAdapter() {
+            public override fun mouseMoved(e : MouseEvent?) {
+                moved.fire(e!!)
+            }
+            public override fun mouseClicked(e : MouseEvent?) {
+                clicked.fire(e!!)
+            }
+            public override fun mouseReleased(e : MouseEvent?) {
+                released.fire(e!!)
+            }
+            public override fun mousePressed(e : MouseEvent?) {
+                pressed.fire(e!!)
+            }
+        }
+
+        component.addMouseListener(listener)
+        component.addMouseMotionListener(listener)
+        component.addMouseWheelListener(listener)
+    }
+
+    fun clicked(body : Component.(MouseEvent) -> Unit) : Mouse {
+        clicked add {e -> component.body(e)}
+        return this
+    }
+
+    fun moved(body : Component.(MouseEvent) -> Unit) : Mouse {
+        moved add {e -> component.body(e)}
+        return this
+    }
+
+    fun released(body : Component.(MouseEvent) -> Unit) : Mouse {
+        released add {e -> component.body(e)}
+        return this
+    }
+
+    fun pressed(body : Component.(MouseEvent) -> Unit) : Mouse {
+        pressed add {e -> component.body(e)}
+        return this
+    }
+
+    val done : Component
+        get() = component
+}
+
+////
 
 fun Timer.repeatEvery(period : Long, initialDelay : Long = 0, taskBody : TimerTask.() -> Unit) : TimerTask {
     val task = object : TimerTask() {
